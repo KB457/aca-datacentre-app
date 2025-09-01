54040fdea47eab6954366ef22d7d6bcaf4fecb0d
BuildTools <unconfigured@null.spigotmc.org>
2025-09-01 12:34:22 +0100

    Modify Readme

--- terraform/modules/az_container_registry/output.tf
+++ terraform/modules/az_container_registry/output.tf
@@ -4,7 +4,7 @@ output "container_registry_id" {
 # The unique resource ID of the Azure Container Registry (ACR).
 # Example format:
 # /subscriptions/<sub_id>/resourceGroups/<rg_name>/providers/Microsoft.ContainerRegistry/registries/<acr_name>
-# Useful for role assignments when you need to give permissions to this registry.
+
 
 
 
@@ -17,4 +17,4 @@ output "container_registry_login_server" {
 ## The login server URL of the ACR.
 # Example format:
 # <acr_name>.azurecr.io
-# This is the address your container app or pipeline will use to push/pull Docker images.
+
